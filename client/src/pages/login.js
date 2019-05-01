afe6ed556c8f951efc8117970d72ef067c358c80
smessiah777 <smessiah17@gmail.com>
2019-05-01 14:18:55 -0500

    login page branding styles created

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 class Login extends Component {
   render() {
     return (
       <section className="section-mgt">
-        <div className="container" />
+        <div className="container">
+          <div className="brand-container">
+            <img
+              className="logo logo-login"
+              src={require("../assets/img/mgt-logo.png")}
+            />
+            <h1 className="mgt-h1">DECK BUILDER</h1>
+          </div>
+        </div>
       </section>
     );
   }
