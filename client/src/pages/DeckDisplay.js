fccdb2a9a86441b4e5c880cb5c2bc2af2c565faa
davemeneses <meneses.coffee@gmail.com>
2019-05-01 14:25:13 -0500

    deck route works, still working on the contect of it

---
+++ client/src/pages/DeckDisplay.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import CardDisplay from "../components/cardDisplay";
+import { Link } from "react-router-dom";
+
+class DeckDisplay extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Test Deck Display</h1>
+      </div>
+    );
+  }
+}
+
+export default DeckDisplay;
