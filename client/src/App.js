0075f21679a5ecd6aceee414adccfc70a460902d
davemeneses <meneses.coffee@gmail.com>
2019-05-02 12:52:19 -0500

    added a display cards page to render all possible card selections

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/deck_display" component={DeckDisplay} />
       </Switch>
     </Router>
