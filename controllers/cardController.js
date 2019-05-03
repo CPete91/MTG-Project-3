b564b23ac8a2e93fc99c9e15eca19ef849ccb069
davemeneses <meneses.coffee@gmail.com>
2019-05-03 13:09:12 -0500

    front end now connected to back end, made changes to the server and utils/api.js

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,19 +1,17 @@
 const db = require("../models");
 
-
-
 module.exports = {
-    findAll: function (req, res) {
-        db.CardDB
-            .find({})
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    findId: function (req, res) {
-        db.CardDB
-            .find({ tcgplayer_id: parseInt(req.params.id) })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    }
-
-}  
+  findAll: function(req, res) {
+    db.CardDB.find({})
+      .then(dbModel => {
+        console.log("got this back from DB!!!", dbModel[0]);
+        res.json(dbModel);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  findId: function(req, res) {
+    db.CardDB.find({ tcgplayer_id: parseInt(req.params.id) })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
