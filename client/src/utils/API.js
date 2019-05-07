e29545d589ddd3bbe3707f5f6257cbf001c0df24
davemeneses <meneses.coffee@gmail.com>
2019-05-07 13:23:40 -0500

    added routes and functions to save to decks to DB

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,11 @@ export default {
   // gets all cards
   getCards: () => {
     return axios.get("http://localhost:3001/api/cards", { crossdomain: true });
+  },
+
+  // saves deckArray to database
+  submitDeck: deckData => {
+    return axios.post("/api/decks", deckData);
   }
 };
 
