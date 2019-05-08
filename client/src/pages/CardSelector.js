8fa4aa40674c33e2258b0fa8a51679246c7abc89
smessiah777 <smessiah17@gmail.com>
2019-05-08 10:42:12 -0500

    search card by letter code written still need to work on rendering

--- client/src/pages/CardSelector.js
+++ client/src/pages/CardSelector.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CardDisplay from "../components/cardDisplay";
+// import SearchLetterField from "../components/searchByLetterInput";
 import { Link } from "react-router-dom";
 import API from "../utils/API";
 
@@ -15,12 +16,15 @@ import {
   Container
 } from "reactstrap";
 
+import { Form, FormGroup, Label, Input, FormText } from "reactstrap";
+
 class CardSelector extends Component {
   state = {
     cardArray: [],
     deckArray: [],
     startIndex: 0,
-    endIndex: 6
+    endIndex: 6,
+    value: ""
   };
 
   renderCard = () => {
@@ -84,11 +88,61 @@ class CardSelector extends Component {
     }
   };
 
+  handleChange = event => {
+    console.log("letter to serach!!", event.target.value);
+    var searchedCards = [];
+    this.state.cardArray.map(data => {
+      console.log("INSIDE MAP", event.target.value, data.name.charAt(0));
+      if (
+        data.name.charAt(0).toLowerCase() == event.target.value.toLowerCase()
+      ) {
+        console.log("INSIDE IFF");
+        searchedCards.push(data);
+      }
+    });
+    console.log("serached !!", searchedCards);
+    // this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = event => {
+    // alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  };
+
+  letterSearch = () => {
+    console.log("letter serach!!");
+
+    // if (this.state.value === this.state.cardArray.name.charAt(0)) {
+    //   var cards = [];
+
+    //   for (let i = this.state.startIndex; i < this.state.endIndex; i++) {
+    //     cards.push(<CardDisplay card={this.state.cardArray[i]} />);
+    //   }
+    //   console.log("I searched by the first letter dude!");
+
+    //   return cards;
+    // }
+  };
+
   render() {
     console.log(this.state.cardArray);
     return (
       <div>
         <Container>
+          <Form className="form-container">
+            <FormGroup>
+              <Label className="form-label" for="search">
+                Search Card By Letter
+              </Label>
+              <Input
+                name="search-letter"
+                type="text"
+                maxLength="1"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </Form>
           <button onClick={this.handleSort}>sort</button>
           <button name="backClick" onClick={this.handleClick}>
             Back
