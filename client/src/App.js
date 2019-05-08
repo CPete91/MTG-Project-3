985d57c1dc507d6dbc4048788873a1907c08a4a7
SNORISS <samuel.norris@marquette.edu>
2019-05-08 11:23:32 -0500

    deck display

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import Test from "./pages/test";
 
 import DeckDisplay from "./pages/DeckDisplay";
 
-<<<<<<< HEAD
 function App() {
   return (
     <Router>
@@ -21,22 +20,6 @@ function App() {
       </Switch>
     </Router>
   );
-=======
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" render={props => <Login {...props} />} />
-          <Route exact path="/cardselector" component={CardSelector} />
-          <Route exact path="/flip" component={Flip} />
-          <Route exact path="/deck_display" component={DeckDisplay} />
-          <Route exact path="/test" component={Test} />
-        </Switch>
-      </Router>
-    );
-  }
->>>>>>> master
 }
 
 export default App;
