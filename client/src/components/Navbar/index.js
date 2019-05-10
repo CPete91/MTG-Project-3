6c131c3d0bfb52ca41ec32a962782df695e50e04
SNORISS <samuel.norris@marquette.edu>
2019-05-10 11:43:46 -0500

    maybe fixed

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,64 +1,64 @@
 import React from "react";
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
 } from "reactstrap";
 
 export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar className="nav-bar" dark expand="md">
-          <NavbarBrand href="/">
-            <img
-              className="nav-brand"
-              src={require("../../assets/img/mgt-logo.png")}
-            />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto nav-links-style" navbar>
-              <NavItem>
-                <NavLink className="nav-links-style" href="/deckdisplay">
-                  Your Decks
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar className="nav-bar" dark expand="md">
+                    <NavbarBrand href="/">
+                        <img
+                            className="nav-brand"
+                            src={require("../../assets/img/mgt-logo.png")}
+                        />
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto nav-links-style" navbar>
+                            <NavItem>
+                                <NavLink className="nav-links-style" href="/deckdisplay" target="_parent">
+                                    Your Decks
                 </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-links-style" href="/cardselector">
-                  New Deck
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-links-style" href="/cardselector" target="_parent">
+                                    New Deck
                 </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-links-style" href="/">
-                  Logout
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-links-style" href="/">
+                                    Logout
                 </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
